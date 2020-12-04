0858ac6146fdf40f8862c7e1ecf532dc52136ca3
TimDeanMoser <tim.moser@uzh.ch>
2020-12-04 11:24:14 +0100

    player and gameboard reviewed

--- gameboard_reviewed.java
+++ gameboard_reviewed.java
@@ -1,14 +1,23 @@
 import java.util.Random;
 import static java.lang.Math.*;
 
-
-// This Class represents the gameboard for a game of Snakes & Ladders
-// Holds the board as an array of Squares with distributed Snakes, Ladders, Normal Squares, Start/End Squares
+/**
+ * This Class represents the gameboard for a game of Snakes & Ladders
+ * @author David Steiger
+ */
 public class Gameboard {
-    // Holds the field of Squares
-    Square[] field;
-    //Constructor
-    public Gameboard(int numberOfSquares) {
+    /**
+     * Holds the board as an array of Squares
+     */
+    protected Square[] field;
+
+    /**
+     * Create a Gameboard of desired size and print to the layout to console
+     *
+     * @param amount The number of desired squares
+     * @void
+     */
+    public Gameboard(final int numberOfSquares) {
         // Initialize field of squares
         this.field = new Square[numberOfSquares];
         // Populate Gameboard
@@ -17,28 +26,32 @@ public class Gameboard {
         System.out.println(this);
     }
 
-    // Initialize Gameboard with given ammount of Snake & Ladder Squares
-    private void initGameboard(int snakesAndLadders) {
+    /**
+     * Initialize Gameboard with given ammount of Snake & Ladder Squares
+     *
+     * @param amount The number of desired Snake & Ladder Squares
+     * @void
+     */
+    private void initGameboard(final int amount) {
+        int snakesAndLadders = in ;
         //Shuffle squares (without start and end, which should be fixated)
-        int[] shuffledSquares = shuffleArray(2, field.length - 1);
+        final int[] shuffledSquares = shuffleArray(2, field.length - 1);
         // Assign fields that are going to be snakes or ladders
         int assignedFields = 0;
         while (snakesAndLadders > 0) {
             // Get start and end square of Snake/Ladder
-            int start;
-            int end;
-            start = Math.min(shuffledSquares[assignedFields], shuffledSquares[assignedFields + 1])
-            end = Math.max(shuffledSquares[assignedFields], shuffledSquares[assignedFields + 1])
+            final int start = min(shuffledSquares[assignedFields], shuffledSquares[assignedFields + 1]);
+            final int end = max(shuffledSquares[assignedFields], shuffledSquares[assignedFields + 1]);
             //alternate snakes and ladder creation
             //create snake square + normal square
             if (snakesAndLadders % 2 == 1) {
                 //Snakes have inverted start/end
-                field[end - 1] = new SnakeSquare(end, (end - start));
+                field[end - 1] = new SnakeSquare(end, end - start);
                 field[start - 1] = new NormalSquare(start);
             }
             //create ladder square + normal square
             else {
-                field[start - 1] = new LadderSquare(start, (end - start));
+                field[start - 1] = new LadderSquare(start, end - start);
                 field[end - 1] = new NormalSquare(end);
             }
             // Increase indices
@@ -60,40 +73,55 @@ public class Gameboard {
     // Helpers
     //
 
-    // Calculate a good amount of Snake and Ladder fields given the board size
-    private int calculateNumberOfSnakesAndLadders(int numberOfSquares) {
+    /**
+     * Calculate a good amount of Snake and Ladder fields given the board size
+     *
+     * @param numberOfSquares The total number of squares of the field
+     * @return A good amount of Snake and Ladder fields
+     */
+    private int calculateNumberOfSnakesAndLadders(final int numberOfSquares) {
         return (numberOfSquares - 2) / 3;
     }
 
-    // Shuffle and return an array such that each Gameboard is random
-    private int[] shuffleArray(int a, int b) {
-        Random rgen = new Random();
-        int size = b - a + 1;
+    /**
+     * Shuffle and return an array such that each Gameboard is random
+     *
+     * @param start index
+     * @param end index
+     * @return A shuffled array, containing integers from start to end in 1 step intervals
+     */
+    private int[] shuffleArray(final int start, final int end) {
+        final Random rgen = new Random();
+        final int size = end - start + 1;
         int[] array = new int[size];
         // Create an array counting from a to b in 1 step intervals
         for (int i = 0; i < size; i++) {
-            array[i] = a + i;
+            array[i] = start + i;
         }
         //Shuffle this array
         for (int i = 0; i < array.length; i++) {
-            int randomPosition = rgen.nextInt(array.length);
-            int temp = array[i];
+            final int randomPosition = rgen.nextInt(array.length);
+            final int temp = array[i];
             array[i] = array[randomPosition];
             array[randomPosition] = temp;
         }
         return array;
     }
 
-    // Print the Gameboard
+    /**
+     * Override toString() for printing the gameboard
+     *
+     * @return String that represents the layout of the gameboard
+     */
     @Override
     public String toString() {
         String output;
         // Greeting
         output += "Let the game begin! Your board looks like this: \n";
-        for (Square s: field) {
+        for (final Square s: field) {
             // Every square type overrides print function differently
-            System.out.println(s)
+            output += s.toString();
         }
-        return output
+        return output;
     }
 }
--- player_reviewed.java
+++ player_reviewed.java
@@ -1,76 +1,83 @@
+/**
+ * This Class represents the player piece for a game of Snakes & Ladders
+ *
+ * @author David Steiger
+ */
+public class player {
+    /**
+     * Holds the position of the player
+     */
+    int position;
+    /**
+     * Holds the name of the player
+     */
+    String name;
 
-public class player
-{
-	int position;
-	String name;
-   
-   //constructor
-	public player(String name) 
-	{ 
-		this.name = name; 
-		this.position = 1; 
-	}
-   
-   //this function moves the player to a different position depending on the die roll
-	public void move(int steps, gameboard gameboard) 
-	{
-      //free the square the player is on before moving
-      gameboard.field[this.position-1].occupied = false;
-      
-      //"underflow" protection
-      if(this.position+steps<1)
-      {
-         this.position = 1;
-         return;
-      }
-      
-      
-      //"overflow" protection
-      if(this.position+steps>gameboard.squares)
-      {         
-         this.move((((gameboard.squares-this.position)*2)-steps),gameboard);
-         return;      
-      }
-      
-      
-      //Different cases for different types of squares
-      //squares with no moving properties
-		if (gameboard.field[this.position+steps-1].type=="snake_square") //checks if snake
-      {
-         this.position+=steps;
-         System.out.println("Oh no, you landed on a snake! \n");
-         this.move(((snake_square)(gameboard.field[this.position-1])).steps_back*(-1), gameboard);
-         return;
-      }
-      else if (gameboard.field[this.position+steps-1].type=="ladder_square") //checks if ladder
-      {
-         this.position+=steps;         
-         System.out.println("Good job, you landed on a ladder! \n");
-         this.move(((ladder_square)(gameboard.field[this.position-1])).steps_forward, gameboard);
-         return;
-      }
-      else
-      {
-         this.position+=steps;
-      }
-      
-      
-      if (gameboard.field[this.position-1].type=="normal_square"| //checks if normal or endsquare is occupied
-               gameboard.field[this.position-1].type=="end_square")
-      {
-         if(gameboard.field[this.position-1].occupied==true) //if player x lands on another player y, player x returns to field 1
-         {
-            this.position=1;
-            System.out.println("Oh no, you landed on another player! Back to square one! \n");
-         }
-         else //else the player occupies the field he/she moved on
-         {
-            gameboard.field[this.position-1].occupied=true;
-         }
-      }
-      
-      return;
-      
-	}
+    /**
+     * Initialize a player with the desired name and at position 1 on the gameboard
+     *
+     * @param name Desired name of Player
+     * @void
+     */
+    public player(String name) {
+        this.name = name;
+        this.position = 1;
+    }
+
+    /**
+     * Move the player on the gameboard
+     *
+     * @param steps The result of a dice roll
+     * @param gameboard The gameboard the player is playing on
+     * @void
+     */
+    public void move(int steps, Gameboard gameboard) {
+        // Free the square the player is on before moving
+        gameboard.field[this.position - 1].occupied = false;
+        // Board Underflow protection
+        if (this.position + steps < 1) {
+            this.position = 1;
+            return;
+        }
+        // Board Overflow protection
+        if (this.position + steps > gameboard.squares) {
+            this.move((((gameboard.squares - this.position) * 2) - steps), gameboard);
+            return;
+        }
+        // Depending on the square the player lands on, we move again
+        Square futurePosition = gameboard.field[this.position + steps - 1];
+        // Checks if snake
+        if (futurePosition.type == "snake_square") {
+            this.position += steps;
+            System.out.println("Oh no, you landed on a snake! \n");
+            this.move(((snake_square)(futurePosition)).steps_back * (-1), gameboard);
+            return;
+        }
+        // Checks if ladder
+        else if (futurePosition.type == "ladder_square") {
+            this.position += steps;
+            System.out.println("Good job, you landed on a ladder! \n");
+            this.move(((ladder_square)(futurePosition)).steps_forward, gameboard);
+            return;
+        }
+        // Normal square
+        else {
+            this.position += steps;
+        }
+        // Checks if landing on occupied field
+        newPosition = gameboard.field[this.position-1]
+        if (newPosition.type == "normal_square" || newPosition.type == "end_square") {
+            // If player x lands on another player y, player x returns to field 1
+            if (newPosition.occupied == true) {
+                this.position = 1;
+                System.out.println("Oh no, you landed on another player! Back to square one! \n");
+            }
+            // Else the player occupies the field he/she moved on
+            else {
+                newPosition.occupied = true;
+            }
+        }
+        return;
+    }
 
 }
